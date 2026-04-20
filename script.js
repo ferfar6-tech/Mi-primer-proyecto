c93e39654de44d6f7eb0449b94a44429245a00fe
ferfar6-tech <ferfar6@gmail.com>
2026-04-20 16:04:32 -0300

    Agregar botones para restar y reiniciar contador

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const counterElement = document.getElementById('counter');
+const decrementButton = document.getElementById('decrement-btn');
 const incrementButton = document.getElementById('increment-btn');
+const resetButton = document.getElementById('reset-btn');
 
 let count = 0;
 
+const updateCounter = () => {
+  counterElement.textContent = count;
+};
+
 incrementButton.addEventListener('click', () => {
   count += 1;
-  counterElement.textContent = count;
+  updateCounter();
+});
+
+decrementButton.addEventListener('click', () => {
+  count -= 1;
+  updateCounter();
+});
+
+resetButton.addEventListener('click', () => {
+  count = 0;
+  updateCounter();
 });
